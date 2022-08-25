4c45b1bd1af280e77fc3e73523e49a4bd6f5b31e
emsweetland <emmaksweetland@gmail.com>
2022-08-25 16:46:57 -0500

    images on dom!!

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const axios = require('axios');
 
 const router = express.Router();
 
 // return search results
 router.get('/', (req, res) => {
-    axios.get(`api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=cats&limit=10`)
+    console.log('in router get')
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=cats&limit=10`)
     .then(response => {
+        console.log(response.data)
         res.send(response.data)}
     ).catch(err => {
         console.error('Search Results GET', err);
